fa2abd56f13077287df13979e9d20e27de6c10b2
Coco <admin@Makhosi-MacBook-Pro.local>
2023-10-24 20:02:13 +0100

    assignments

--- js/index.js
+++ js/index.js
@@ -4,15 +4,14 @@
 
 
     for (i=1; i<=100; i++){
-        if(i %3 === 0){
-            console.log("fizz");
-        }else if( i %5 ===0){
-            console.log("buzz");
-        }else if ( i % 15 === 0){
+        if(i %15 ===0){
             console.log("fizzBuzz")
-        } else 
+        }else if(i %3 === 0){
+            console.log("fizz");
+        } else if ( i %5 ===0){
+            console.log("buzz");
+        }else 
         console.log(i);
-
     };
     
 
@@ -66,3 +65,5 @@ function listOfFruits () {
 listOfFruits();
 
 // *** End of assignment.
+
+// Week 3 Assignment.
